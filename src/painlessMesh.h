306420b4164e9bb31770735010ae66f59b534488
Doanh in Windowns <doanhlv@softdreams.vn>
2018-04-16 12:00:21 +0700

    complete remove espInterface.h file

--- src/painlessMesh.h
+++ src/painlessMesh.h
@@ -12,10 +12,11 @@
 #include <functional>
 #include <memory>
 using namespace std;
-#include "espInterface.h"
 #ifdef ESP32
+#include <WiFi.h>
 #include <AsyncTCP.h>
 #elif defined(ESP8266)
+#include <ESP8266WiFi.h>
 #include <ESPAsyncTCP.h>
 #endif // ESP32
 
--- src/painlessMeshSTA.h
+++ src/painlessMeshSTA.h
@@ -7,7 +7,11 @@
 #define _TASK_STD_FUNCTION
 #include <TaskSchedulerDeclarations.h>
 
-#include "espInterface.h"
+#ifdef ESP32
+#include <WiFi.h>
+#elif defined(ESP8266)
+#include <ESP8266WiFi.h>
+#endif // ESP32
 
 #define SCAN_INTERVAL       10*TASK_SECOND // AP scan period in ms
 
